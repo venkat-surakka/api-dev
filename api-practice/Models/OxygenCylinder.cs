0188b4121d103772557075cfb0f2464edc735690
Venkat Surakka <venkat.surakka@outlook.com>
2021-10-14 23:27:24 +1300

    Added oxygen cylinders API

---
+++ api-practice/Models/OxygenCylinder.cs
@@ -0,0 +1,27 @@
+using System;
+namespace api_practice.Models
+{
+    public class OxygenCylinder
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Brand
+        {
+            get;
+            set;
+        }
+        public double Capacity
+        {
+            get;
+            set;
+        }
+        public double Price
+        {
+            get;
+            set;
+        }
+    }
+}
